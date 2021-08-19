033155a65b33c9e37b6b95e85df9ed0e6204aca4
Kartikay Chiranjeev Gupta <72543830+Chiranjeev-Kartik@users.noreply.github.com>
2021-08-19 12:08:46 +0530

    Updated and Documented
    Now updated to a scalable and documented solver.

--- main.py
+++ main.py
@@ -1,123 +1,201 @@
-#!/usr/bin/env python3
-# Example board
-board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
-# New Board <- Fill the values to prepare a new board.
-# board = [
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0]
-# ]
-
-row_len = len(board[0])
-column_len = len(board)
+"""
+Author: Kartikay Chiranjeev Gupta.
+Last Modified: 8/1/2021
+"""
 element_path = []
 
 
-def board_rep(board_):  # To represent board in a pretty format.
-    for i in range(0, column_len):
+def board_rep(board, row, col):
+    """
+    Represents the board in pretty format.
+    :param board: 2-D list.
+    :param row: Number of rows.
+    :param col: Number of columns.
+    :return: None.
+    """
+    for i in range(col):
         print("")
-        for j in range(0, row_len):
-            if (j + 1) % 3 == 0 and (j + 1) != row_len:
-                print(f" {board_[i][j]} |", end="")
+        for j in range(row):
+            if (j + 1) % 3 == 0 and (j + 1) != row:
+                print(f" {board[i][j]} |", end="")
                 continue
-            print(f" {board_[i][j]} ", end="")
-        if (i + 1) % 3 == 0 and (i + 1) != column_len:
+            print(f" {board[i][j]} ", end="")
+        if (i + 1) % 3 == 0 and (i + 1) != col:
             print("\n- - - - - - - - - - - - - - -", end="")
 
 
-def row_check(board_):  # To check that No number repeat in a row.
-    for row in board_:
-        row_elements = []
-        for element in row:
-            if element == 0:
+def take_dim():
+    """
+    Asks the user for Sudoku board dimension.
+    [Note: The Dimension of board must be multiple of 3.]
+    :return: row x column.
+    """
+    [row, col] = list(map(int, input('Enter board dimension (row x column): ').strip().split(' ')))
+    if row % 3 != 0 and col % 3 != 0:
+        print('Invalid Dimensions!')
+        return take_dim()
+    else:
+        return row, col
+
+
+def take_board(row, col):
+    """
+    Asks user to enter board elements.
+    [Note: elements must be in range 0-9, Where 0 represents missing value.]
+    :param row: Number of rows.
+    :param col: Number of columns.
+    :return: 2-D list. Representing board elements.
+    """
+    board = [[] for _ in range(row)]
+    for r in range(row):
+        for c in range(col):
+            e = int(input(f'Element {r + 1}th row and {c + 1}th column: '))
+            while not 0 <= e <= 9:
+                print('Invalid number!')
+                e = int(input(f'Element {r + 1}th row and {c + 1}th column: '))
+            board[r].append(e)
+    return board
+
+
+def row_check(board, row, col):
+    """
+    Returns False if element repeat in row.
+    :param board: 2-D list.
+    :param row: Number of rows.
+    :param col: Number of columns.
+    :return: Bool.
+    """
+    for r in range(row):
+        row_element = []
+        for c in range(col):
+            if board[r][c] == 0:
                 continue
-            if element in row_elements:
+            if board[r][c] in row_element:
                 return False
             else:
-                row_elements.append(element)
+                row_element.append(board[r][c])
     return True
 
 
-def column_check(board_):  # To check No number repeat in a column.
-    for i in range(0, column_len):
-        column_elements = []
-        for j in range(0, row_len):
-            if board_[j][i] == 0:
+def col_check(board, row, col):
+    """
+    Returns False if element repeat in column.
+    :param board: 2-D list.
+    :param row: Number of rows.
+    :param col: Number of columns.
+    :return: Bool.
+    """
+    for c in range(col):
+        col_element = []
+        for r in range(row):
+            if board[r][c] == 0:
                 continue
-            if board_[j][i] in column_elements:
+            if board[r][c] in col_element:
                 return False
             else:
-                column_elements.append(board_[j][i])
+                col_element.append(board[r][c])
     return True
 
 
-def valid_no(board_):  # To check No number is more than 9.
-    for i in range(0, 9):
-        for j in range(0, 9):
-            if board_[i][j] > 9:
-                return False
-    return True
-
-
-def box_checker(board_):  # To check No number repeat in a Box.
-    for x in [0, 1, 2]:
-        for i in [0, 1, 2]:
+def box_check(board, row, col):
+    """
+    Returns False if any element is repeated in 3 x 3 box.
+    :param board: 2-D list.
+    :param row: Number of rows.
+    :param col: Number of columns.
+    :return: Bool.
+    """
+    for r in range(row // 3):
+        for c in range(col // 3):
             elements = []
-            for j in range(i * 3, i * 3 + 3):
-                for k in range(x * 3, x * 3 + 3):
-                    if board_[j][k] == 0:
+            for j in range(c * 3, c * 3 + 3):
+                for k in range(r * 3, r * 3 + 3):
+                    if board[j][k] == 0:
                         continue
-                    if board_[j][k] in elements:
+                    if board[j][k] in elements:
                         return False
                     else:
-                        elements.append(board_[j][k])
+                        elements.append(board[j][k])
     return True
 
 
-def solver(board_, i=0, j=0):  # To Fill number according to rows, columns, and boxes.
-    # ( Using the back tracking algorithm)
+def valid_no(board, row, col):
+    """
+    Returns False if any element is more than 9.
+    :param board: 2-D list.
+    :param row: Number of rows.
+    :param col: Number of columns.
+    :return: Bool.
+    """
+    for i in range(row):
+        for j in range(col):
+            if board[i][j] > 9:
+                return False
+    return True
+
+
+def solve_for_one(board, row, col, i=0, j=0):
+    """
+    Update value of only one element specified by i and j. And moves backward if no value satisfy at i, j.
+    :param board: 2-D list.
+    :param row: Number of rows.
+    :param col: Number of columns.
+    :param i: ith row.
+    :param j: jth column.
+    :return: updated board and i, j
+    """
     global element_path
-    if board_[i][j] == 0:
+    if board[i][j] == 0:
         element_path.append((i, j))
-        board_[i][j] += 1
-        while not (row_check(board_) and column_check(board_) and box_checker(board_) and valid_no(board_)):
-            if board_[i][j] < 9:
-                board_[i][j] += 1
+        board[i][j] += 1
+        while not (row_check(board, row, col) and col_check(board, row, col) and box_check(board, row, col)
+                   and valid_no(board, row, col)):
+            if board[i][j] < 9:
+                board[i][j] += 1
             else:
-                board_[i][j] = 0
+                board[i][j] = 0
                 element_path.pop(-1)
                 i, j = element_path[-1]
-                board_[i][j] += 1
+                board[i][j] += 1
                 continue
-    return board_, i, j
+    return board, i, j
 
 
-def result(board_):  # To Move in each column and row.
+def solver(board, row, col):
+    """
+    Moves to all position until the board is solved.
+    :param board: 2-D list.
+    :param row: Number of rows.
+    :param col: Number of columns.
+    :return: Final solved board.
+    """
     i = 0
     j = 0
-    while i < 9:
-        while j < 9:
-            board_, i, j = solver(board_, i, j)
+    while i < row:
+        while j < col:
+            board, i, j = solve_for_one(board, row, col, i, j)
             j += 1
         i += 1
         j = 0
-    return board_
+    return board
 
 
-board_rep(result(board))
+row_, col_ = take_dim()
+board_ = take_board(row_, col_)
+# row, col = 9, 9
+# board = [
+#     [5, 3, 0, 0, 7, 0, 0, 0, 0],
+#     [6, 0, 0, 1, 9, 5, 0, 0, 0],
+#     [0, 9, 8, 0, 0, 0, 0, 6, 0],
+#     [8, 0, 0, 0, 6, 0, 0, 0, 3],
+#     [4, 0, 0, 8, 0, 3, 0, 0, 1],
+#     [7, 0, 0, 0, 2, 0, 0, 0, 6],
+#     [0, 6, 0, 0, 0, 0, 2, 8, 0],
+#     [0, 0, 0, 4, 1, 9, 0, 0, 5],
+#     [0, 0, 0, 0, 8, 0, 0, 7, 9]
+# ]  # Test board. (Zero represent missing values)
+
+
+board_rep(board_, row_, col_)
+print("\n\nSolving board...")
+board_rep(solver(board_, row_, col_), row_, col_)
